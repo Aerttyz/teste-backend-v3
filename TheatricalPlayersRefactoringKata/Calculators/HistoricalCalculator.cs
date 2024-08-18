0b51f6ebb19beccfbfb734e87672940d4aaefdc0
Aerttyz <alesandrosilva2004@gmail.com>
2024-08-18 15:26:50 -0300

    feat: Add HistoricalCalculator class for calculating performance amount and volume credits

---
+++ TheatricalPlayersRefactoringKata/Calculators/HistoricalCalculator.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace TheatricalPlayersRefactoringKata.Calculators
+{
+    public class HistoricalCalculator : IPerformanceCalculator
+    {
+        private readonly TragedyCalculator _tragedyCalculator;
+        private readonly ComedyCalculator _comedyCalculator;
+
+        public HistoricalCalculator(TragedyCalculator tragedyCalculator, ComedyCalculator comedyCalculator)
+        {
+            _tragedyCalculator = tragedyCalculator;
+            _comedyCalculator = comedyCalculator;
+        }
+
+        public int CalculateAmount(Performance performance, Play play)
+        {
+            return _tragedyCalculator.CalculateAmount(performance, play) + _comedyCalculator.CalculateAmount(performance, play);
+        }
+
+        public int CalculateVolumeCredits(Performance performance, Play play)
+        {
+            return _tragedyCalculator.CalculateVolumeCredits(performance, play);
+        }
+    }
+}
