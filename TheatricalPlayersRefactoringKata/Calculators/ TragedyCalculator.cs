5062e0a318d526e31c422e0f528789c5b4956e8f
Aerttyz <alesandrosilva2004@gmail.com>
2024-08-18 17:26:38 -0300

    feat: Refactor TragedyCalculator to improve performance amount calculation

--- TheatricalPlayersRefactoringKata/Calculators/ TragedyCalculator.cs
+++ TheatricalPlayersRefactoringKata/Calculators/ TragedyCalculator.cs
@@ -5,15 +5,20 @@ namespace TheatricalPlayersRefactoringKata.Calculators
 {
     public class TragedyCalculator : IPerformanceCalculator
     {
-        public int CalculateAmount(Performance performance, Play play)
-        {
-            int lines = Math.Clamp(play.NumberOfLines, 1000, 4000);
-            int baseAmount = lines / 10;
+        public double CalculateAmount(Performance performance, Play play)
+        {   
+            double result = 0;
+            int lines = Math.Clamp(play.Lines, 1000, 4000);
+            double baseAmount = lines * 10;
+            double additionalAmount = 0;
+
             if (performance.Audience > 30)
             {
-                result += 1000 * (performance.Audience - 30);
+                additionalAmount = 1000 * (performance.Audience - 30);
             }
-            return baseAmount;
+            result = baseAmount + additionalAmount;
+
+            return result;
         }
 
         public int CalculateVolumeCredits(Performance performance, Play play)
