39e54a577ff243fcf1943ec844cf426a444a9d4c
Aerttyz <alesandrosilva2004@gmail.com>
2024-08-18 17:26:58 -0300

    feat: Refactor ComedyCalculator to improve performance amount calculation

--- TheatricalPlayersRefactoringKata/Calculators/ComedyCalculator.cs
+++ TheatricalPlayersRefactoringKata/Calculators/ComedyCalculator.cs
@@ -3,21 +3,28 @@ using System;
 
 namespace TheatricalPlayersRefactoringKata.Calculators
 {
-    public interface ComedyCalculator
+    public class ComedyCalculator : IPerformanceCalculator
     {
-        public int CalculateAmount(Performance performance, Play play){
-            int lines = Math.Clamp(play.NumberOfLines, 1000, 4000);
-            int baseAmount = lines / 10;
+        public double CalculateAmount(Performance performance, Play play)
+        {
+            double result = 0;
+            int lines = Math.Clamp(play.Lines, 1000, 4000);
+            double baseAmount = lines * 10;
+            double additionalAmount = 0;
+            
             if (performance.Audience > 20)
             {
-                result += 10000 + 500 * (performance.Audience - 20);
+                additionalAmount = 10000 + 500 * (performance.Audience - 20);
             }
-            result += 300 * performance.Audience;
-            return baseAmount;
+            baseAmount += 300 * performance.Audience;
+            result = baseAmount + additionalAmount;
+            return result;
         }
-        public int CalculateVolumeCredits(Performance performance, Play play){
-            int credits =  Math.Max(performance.Audience - 30, 0);
 
+
+        public int CalculateVolumeCredits(Performance performance, Play play)
+        {
+            int credits = Math.Max(performance.Audience - 30, 0);
             credits += performance.Audience / 5;
             return credits;
         }
