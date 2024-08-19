68f672a19d26bfb4b7692214b01778cc4114cf4d
Aerttyz <alesandrosilva2004@gmail.com>
2024-08-18 22:46:31 -0300

    feat: Add XML printing functionality to StatementPrinter class

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Xml.Linq;
+using System.Text;
 using TheatricalPlayersRefactoringKata.Calculators;
 
 namespace TheatricalPlayersRefactoringKata
@@ -53,5 +56,92 @@ namespace TheatricalPlayersRefactoringKata
             result += String.Format("You earned {0} credits\n", volumeCredits);
             return result;
         }
+       public string PrintXml(Invoice invoice, Dictionary<string, Play> plays)
+        {
+            var totalAmount = 0;
+            var totalVolumeCredits = 0;
+            
+
+            var statement = new XElement("Statement",
+                new XAttribute(XNamespace.Xmlns + "xsi", "http://www.w3.org/2001/XMLSchema-instance"),
+                new XAttribute(XNamespace.Xmlns + "xsd", "http://www.w3.org/2001/XMLSchema"),
+                new XElement("Customer", invoice.Customer),
+                new XElement("Items")
+            );
+            XDocument doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", null),
+                statement
+            );
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayId];
+                var lines = play.Lines;
+
+                if (lines < 1000) lines = 1000;
+                if (lines > 4000) lines = 4000;
+
+               
+                var thisCredits = 0;
+                IPerformanceCalculator calculator;
+
+                var tragedyCalculator = new TragedyCalculator();
+                var comedyCalculator = new ComedyCalculator();
+                var historicalCalculator = new HistoricalCalculator(tragedyCalculator, comedyCalculator);
+
+                switch (play.Type)
+                {
+                    case "tragedy":
+                        calculator = tragedyCalculator;
+                        thisCredits = calculator.CalculateVolumeCredits(perf, play);
+                        break;
+                    case "comedy":
+                        calculator = comedyCalculator;
+                        thisCredits = calculator.CalculateVolumeCredits(perf, play);
+                        break;
+                    case "history":
+                        calculator = historicalCalculator;
+                        thisCredits = calculator.CalculateVolumeCredits(perf, play);
+                        break;
+                    default:
+                        throw new Exception("unknown type: " + play.Type);
+                }
+
+                var thisAmount = calculator.CalculateAmount(perf, play)/100.0;
+
+                statement.Element("Items").Add(
+                    new XElement("Item",
+                        new XElement("AmountOwed", FormatCurrency(thisAmount)),
+                        new XElement("EarnedCredits", thisCredits), 
+                        new XElement("Seats", perf.Audience)
+                    )
+                );
+
+                totalAmount += (int)(thisAmount*100); 
+                totalVolumeCredits += thisCredits;
+            }
+
+            statement.Add(new XElement("AmountOwed", FormatCurrency(totalAmount/100.0)));
+            statement.Add(new XElement("EarnedCredits", totalVolumeCredits));
+            
+            doc.Save("statement.xml");
+
+            string xmlContent = File.ReadAllText("statement.xml");
+            
+
+            return File.ReadAllText("statement.xml");
+        }
+
+        private string FormatCurrency(double amount)
+        {
+            if (amount % 1 == 0)
+            {
+                return amount.ToString("F0", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                return amount.ToString("F1", CultureInfo.InvariantCulture);
+            }
+        }
+
     }
 }
